f599675ab667b7714481afe214e788c339abe00b
swang <swang@MacBook-sh.local>
2021-11-25 22:35:04 +0900

    add print part

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 15:32:48 by swang             #+#    #+#             */
-/*   Updated: 2021/11/21 02:26:25 by swang            ###   ########.fr       */
+/*   Updated: 2021/11/25 22:20:48 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,15 @@ int	main(int argc, char *argv[], char *envp[])
 {
 	char			*line;
 	t_info			info;
-	t_parse_list	*parse_list;
 
-	//추후 시그널처리 추가 예정
+	//sig_init();
 	//print_ascii_art();
 	(void)argc;
 	(void)argv; //unused 어쩌고 에러땜에 추가
-	init_info(&info);
-	info.path = get_path(envp);
-	if (info.path == NULL)
-		printf("Failed to find PATH in envp\n");
+	init_info(&info, envp);
 	while(42)
 	{
+		make_env_list(&info);
 		line = readline("GAEPOSHELL$ ");
 		if (line || *line)
 			add_history(line);
@@ -36,35 +33,33 @@ int	main(int argc, char *argv[], char *envp[])
 			printf("exit\n");
 			exit(0);
 		}
-		parse_list = run_parsing(line, &info); //입력받은 라인과 info구조체넘겨서 파싱하
-		if (info.token != NULL) //token 한글자씩 찍는 테스트문
+		
+		//info.파서리스트 = run_parsing(line, &info);
+		//파서구조체를 지워버려서 실행이 안됩니다~ 추가해주세용
+
+		// 실행파트
+		//환경변수 리스트초기화
+		//렉서랑 파서 리스트 초기화
+		//line과 관련된 애들 정보 지우기 (새 라인을 받아올거니까)
+		free(line);
+	}
+	//인포에있는거 초기화...
+	//프로그램 종료 전 실행해야하는 것들
+	return (0);
+}
+/*
+		printf("-----in main : print token-----\n");
+		if (info.token != NULL) //token 찍는 테스트문
 		{
 			int x = 0;
 			while (info.token[x])
 			{
-				printf("%s\n", info.token[x]);
-				/*
-				int y = 0;
-				while (info.token[x][y])
-				{
-					printf("%c", info.token[x][y]);
-					y++;
-				}
-				printf(" ");
-				*/
+				printf("	[%d:%s:(%d)]\n", x, info.token[x], info.real[x]);
 				x++;
 			}
+			printf("-------------end main-------------\n");
 		}
-		// 실행파트
-		// line과 관련된 애들 정보 지우기 (새 라인을 받아올거니까)
-		free(line);
-	}
-	//프로그램 종료 전 실행해야하는 것들
-	return (0);
-}
-
-
-
+		*/
 /*
 if (ft_strncmp(line, "path", 4) == 0)
 {
