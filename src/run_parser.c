e919ba0751bf06d257cb853bc50e99ecaed37e70
swang <swang@MacBook-sh.local>
2021-12-07 17:01:01 +0900

    update variable name

--- src/run_parser.c
+++ src/run_parser.c
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 15:44:02 by swang             #+#    #+#             */
-/*   Updated: 2021/12/06 21:24:51 by swang            ###   ########.fr       */
+/*   Updated: 2021/12/07 14:07:43 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	run_parser(t_info *info)
 	t_lexical_node *p;
 
 	info->parse_list = (t_parse_list *)ft_calloc(1, sizeof(t_parse_list));
-	int	i;
+	int	count;
 	int	node_i;
-	i = 0;
+	count = 0;
 	node_i = 0;
 	p = info->lex_list->head;
 	info->lex_list->curr = info->lex_list->head;
@@ -27,38 +27,36 @@ void	run_parser(t_info *info)
 	{
 		if (p->type == PIPE)
 		{
-			make_parse_node(p, info, i, node_i); // 파이프 전까지의 데이터를 담아서
-			i = -1;
+			make_parse_node(info, count, node_i); // 파이프 전까지의 데이터를 담아서
+			info->lex_list->curr = p->next;
+			// 파이프 다음부터 만들어야 하므로 curr은 포인터 다음의 렉서노드를 가리키고 있음
+			count = 0;
 			node_i++;
 		} //i는 여기를 지나면서 0이 됨.
-		i++;
-		if (!(p->next))
-			break ;
+		else
+			count++;
 		p = p->next;
 	}
-	make_parse_node(p, info, i, node_i);
+	make_parse_node(info, count, node_i);
 }
 
-void	make_parse_node(t_lexical_node *p, t_info *info, int i, int node_i)
+void	make_parse_node(t_info *info, int count, int node_i)
 {
-//	i는 렉서노드 개수
-//	node_i는 파서인덱스
 	t_parse_node *new;
 	int idx;
-//{ㅁ->ㅁ->ㅁ} -> | -> {ㅁ-> ㅁ->ㅁ}
 	idx = 0;
 	new = (t_parse_node *)ft_calloc(1, sizeof(t_parse_node));
 	new->index = node_i;
-	new->cmd = (char **)ft_calloc(i + 1, sizeof(char *));
-	new->lex = (int *)ft_calloc(i + 1, sizeof(int));
-	while (i)
+	new->cmd = (char **)ft_calloc(count + 1, sizeof(char *));
+	new->lex = (int *)ft_calloc(count + 1, sizeof(int));
+	while (idx < count)
 	{
-//		new->cmd[idx] = (char *)malloc(sizeof(char) * (ft_strlen(info->lex_list->curr->value)));
 		new->cmd[idx] = info->lex_list->curr->value;
 		new->lex[idx] = info->lex_list->curr->type;
-		i--;
 		idx++;
 		info->lex_list->curr = info->lex_list->curr->next;
+		//첫 진입시 curr은 머리, 머리부터 count개수만큼 만든다.
+		//재진입시 curr은 이전 파이프의 다음노드부터 count개수만큼 (파이프와 파이프 사이를 만드는 중)
 	}
 	if (info->parse_list->head == 0)
 	{
@@ -71,7 +69,6 @@ void	make_parse_node(t_lexical_node *p, t_info *info, int i, int node_i)
 		new->prev = info->parse_list->tail;
 		info->parse_list->tail = new;
 	}
-	info->lex_list->curr = p->next; //p는 파이프가 나오는 위치
 	new = 0;
 }
 	// curr 이 가리키는 부분부터 i 개만큼 렉서 노드를 탐색
