00f1cafec52bb46b3b0e7f426b1db30650b88e89
swang <swang@c1r5s5.42seoul.kr>
2021-12-10 19:54:54 +0900

    💩클러스터에서 리팩토링 중 백업 (집에 가서 마저 해야함)

--- src/run_tokenizer.c
+++ src/run_tokenizer.c
@@ -6,18 +6,44 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 00:59:22 by swang             #+#    #+#             */
-/*   Updated: 2021/11/25 21:35:53 by swang            ###   ########.fr       */
+/*   Updated: 2021/12/10 18:55:29 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+static int	check_sign(char *str, t_info *info)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		check_quote_flag(str[i], info);
+		if (!(info->quote & SQ) && str[i] == '$')
+			return (1); //치환해야하는거 하나라도 있음
+		i++;
+	}
+	return (0);
+}
+
 char	**run_tokenizer(char *line, t_info *info)
 {
 	char	**arr;
+	char	*tmp;
+	int		i;
 
+	i = 0;
 	arr = divide_line(line, info);
-	convert_env(arr, info);
-	trim_quote(arr, info);
+	while(arr[i])
+	{
+		if (check_sign(arr[i], info) == 1)
+		{
+			tmp = arr[i];
+			arr[i] = convert_env(arr[i], info);
+			free(tmp);
+		}
+	}
+	//arr = trim_quote(arr, info);
 	return (arr);
 }
--- src/tokenizer_convert_env.c
+++ src/tokenizer_convert_env.c
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 21:31:17 by swang             #+#    #+#             */
-/*   Updated: 2021/11/25 22:11:38 by swang            ###   ########.fr       */
+/*   Updated: 2021/12/10 19:47:21 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,76 +32,115 @@ static char	*change_shell_var(char *str, t_info *info)
 	return (ret);
 }
 
-static char	*change_env_var(char *str, t_info *info)
+//------------------------------아래 새로운 함수---
+
+static char	*change_env_var(char *str, int *idx, t_info *info)
 {
 	t_env_node	*p;
 	int		len;
-	char	*s1;
-	char	*s2;
+	char	*head;
+	char	*tail;
 	char	*ret;
 
-	p = info->env_head;
-	len = 0;
-	while (p != 0)
+	p = info->env_list->head;
+	//str[idx] = $
+	*idx += 1;
+	len = *idx;
+	while (ft_isalnum(str[*idx]) == 1 || str[len] == '_')
+		*idx += 1;
+	len = *idx - len;
+	if (len == (int)ft_strlen(p->env_arr[0]) && !(ft_strncmp(str, p->env_arr[0], len)))
 	{
-		//len = 문자열 str에서 띄어쓰기나 기타등등 환경변수인부분
-		while (ft_isalnum(str[len]) == 1 || str[len] == '_')
-			len++;
-		if (!(ft_strncmp(str, p->env_arr[0], len)))
-		{
-			s1 = p->env_arr[1];
-			s2 = ft_substr(str, len, ft_strlen(str + len));
-			ret = ft_strjoin(s1, s2);
+
+			head = p->env_arr[1];
+			tail = ft_substr(str, len, ft_strlen(str + len));
+			ret = ft_strjoin(head, tail);
 			free(s2);
 			return (ret);
-		}
+	}
 		p = p->next;
 	}
 	//환경변수랑 겹치는 게 없을땐 빈문자열반환 (이상한환경변수는 사라짐)
-	ret = ft_strdup("");
+	ret = ft_substr(str, len, ft_strlen(str + len));
 	return (ret);
 }
 
-void	convert_env(char **arr, t_info *info)
+
+static char	*trim_sign(char *str, int *idx)
+{
+	//$"ㅁㅇㄹ" 이렇게 들어오면 $ 제거해주기
+	char	*head;
+	char	*tail;
+	char	*ret;
+
+	head = ft_substr(str, 0, *idx);
+	tail = ft_substr(str, *idx + 1, ft_strlen(str + (*idx) + 1));
+	ret = ft_strjoin(head, tail);
+	free (head);
+	free (tail);
+	*idx -= 1;
+	return (ret);
+}
+
+static char	*convert_env2(char *str, int *i, t_info *info)
+{
+	char	*head;
+	char	*convert;
+	char	*new;
+	int		idx;
+
+	idx = *i;
+	// 제거 / $? / 환경
+	head = ft_substr(str, 0, *i);
+	if (str[idx + 1] && (ft_isquote(str[idx + 1])))
+		convert = trim_sign(str, &idx);
+	else if (str[idx + 1] && str[idx + 1] == '?')
+		convert = change_shell_var(str, &idx, info);
+	else if (str[idx + 1] && (str[idx + 1] == '-' || ft_isalnum(str[idx + 1])))
+		convert = change_env_var(str[idx], &idx, info);
+	else
+		ft_strendl_fd("convert_env2 케이스 추가\n");
+	new = ft_strjoin(head, convert);
+	*i = idx;
+	//$USER$USER i = 0 에서 치환이 완료되면, 5를 가리키고 있어야함
+	return (con);
+}
+
+char	*convert_env(char *str, t_info *info)
 {
 	// 작은따옴표 안에있느건 무시해야함
+	// 환경변수를 치환한 새로운 토큰 한개만 리턴
 	int	i;
-	int	j;
 	char	*s1;
-	char	*s2;
-	char	*temp;
+	char	*s2
+	char	*tmp;
 
 	i = 0;
-	while(arr[i])
+	while(str[i])
 	{
-		j = 0;
-		while(arr[i][j])
+		check_quote_flag(str[i], info); //sq가 켜져있으면 치환 ㄴ
+		if (!(info->quote & SQ) && str[i] == '$')
 		{
-			check_quote_flag(arr[i][j], info); //sq가 켜져있으면 치환 ㄴ
-			if (!(info->quote & SQ) && arr[i][j] == '$' && (ft_isalpha(arr[i][j + 1]) == 1 || arr[i][j + 1] == '?' || arr[i][j + 1] == '_'))
+			// $ + 따옴표 = 제거,  vs 그냥
+			if (str[i + 1] && (ft_isquote(str[i + 1]) || str[i + 1] == '?' || str[i + 1] == '-' || ft_isalnum(str[i])))
 			{
-				s1 = ft_substr(arr[i], 0, j);
-				if (arr[i][j + 1] == '?')
-					s2 = change_shell_var(arr[i] + j, info);
-				else
-					s2 = change_env_var(arr[i] + j + 1, info);
-				temp = arr[i];
-				arr[i] = ft_strjoin(s1, s2);
-				free(s1);
-				free(s2);
-				free(temp);
+				tmp = new;
+				//	치환/제거하고 변경된 i를 받을 수 있도록, 한번에 하나의 환경변수만 처리
+				s1 = ft_substr(str, 0 , i);
+				s2 = convet_env2(str + i, &i, info);
+				if (tmp)
+					free(tmp);
 			}
-			j++;
+			// if문에 해당 안되면 그냥 문자열 처리하고 넘어감
 		}
 		i++;
 	}
+	return (new);
 }
 
 /*
-	p = info->env_head;
-	while (p != 0)
-	{
-		printf("	[%s] : [%s]\n", p->env_arr[0], p->env_arr[1]);
-		p = p->next;
-	}
+//	asdfadf$dahj"kla"$ADFSAA$ssdf 이렇게 있을 때
+	head{asdfadf} / 치환함수($dahj"kla"$ADFSAA$ssdf)
+					치환($dahj) / tail{"kla"$ADFSAA$ssdf}
+
 */
