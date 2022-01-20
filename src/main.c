5aac1a150512ed9bdac78381c3f186dd40820558
swang <swang@c1r4s3.42seoul.kr>
2022-01-20 19:43:29 +0900

    Fix : ctrl-D로 종료 할 때 개행 후 exit 되는 현상 수정

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 15:32:48 by swang             #+#    #+#             */
-/*   Updated: 2022/01/20 14:12:01 by swang            ###   ########.fr       */
+/*   Updated: 2022/01/20 17:53:03 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,17 @@
 
 int g_exit_status;
 
-
 int	check_no_pipe_builtin(t_info *info)
 {
 	int	i;
+	int	*ptr;
 
 	i = 0;
-	while (info->parse_list->head->lex[i] != CMD)
+	ptr = info->parse_list->head->lex;
+	while (ptr[i] && ptr[i] != CMD)
 		i++;
+	if (ptr[i] == 0)
+		return (0);
 	if (info->parse_list->head == info->parse_list->tail && ft_isbuiltin(info->parse_list->head->cmd[i]))
 		return (1);
 	else
@@ -42,46 +45,42 @@ int	main(int argc, char *argv[], char **envp)
 	t_info			info;
 
 	g_exit_status = 0;
-	signal(SIGINT, handle_signal);
-	signal(SIGQUIT, handle_signal);
-	//sig_init();
-	//print_ascii_art();
-	(void)argc;
-	(void)argv; //unused 어쩌고 에러땜에 추가
-	init_info(&info);
-	info.env_list = make_env_list(envp); // PWD:
-	info.old_pwd = get_env_path(info.env_list->head, "OLDPWD");
-	info.home = get_env_path(info.env_list->head, "HOME");
+	sig_init();
+	use_arg(argc, argv);
+	init_info(&info, envp);
 	while(42)
 	{
 		line = readline("GAEPOSHELL$ ");
-		if (*line != '\0')
+		if (line && *line != '\0')
+			add_history(line);
+		if (line == NULL)
 		{
-			if (line)//*line
-				add_history(line);
-			else if (line == NULL)
-			{
-				printf("exit\n");
-				exit(0);
-			}
-			if (sin_error(line))
-				printf("syntax error near unexpected token\n");
-			else if (parsing(line, &info) == -1)
-				printf("syntax error near unexpected token\n");
-			else
-			{
-				if (check_no_pipe_builtin(&info) == 1)
-				{
-					pre_open(&info);
-					ft_isheredoc(&info);
-					run_builtin(info.parse_list->head, &info);
-				}
-				else
-					ft_execute(&info);
-			}
+			ft_putstr_fd("\x1b[1A", 1);
+			ft_putstr_fd("\033[12C", 1);
+			ft_putendl_fd("exit", 1);
+			exit(0);
 		}
-	//	printf("		exit_stat = [%d]\n", info.exit_stat);
-		delete_line(&info, line);
+		else if (*line == '\0')
+		{
+			free(line);
+			continue;
+		}
+		if (sin_error(line))
+			printf("syntax error near unexpected token\n");
+		else if (parsing(line, &info) == -1)
+			printf("syntax error near unexpected token\n");
+		else
+		{
+			if (check_no_pipe_builtin(&info) == 1)
+			{
+				pre_open(&info);
+				ft_isheredoc(&info);
+				run_builtin(info.parse_list->head, &info);
+			}
+			else
+				ft_execute(&info);
+		}
+		free_line(&info, line);
 	}
 	free_info(&info);
 	while (1)
