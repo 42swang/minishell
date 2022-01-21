76ab54970d28bed77dfa6abd7b0fd08b4f0c3885
swang <swang@c1r4s5.42seoul.kr>
2022-01-21 16:25:12 +0900

    미니쉘 안에서 미니쉘 실행 체크

--- src/find_cmd_path.c
+++ src/find_cmd_path.c
@@ -8,6 +8,12 @@ char *find_cmd_path(char **arr, char *cmd)
 	path = 0;
 	if (arr == NULL)
 		return (0);
+	path = ft_strdup(cmd);
+	if (access(path, X_OK | F_OK) == 0) //내가만든 경로가 실행가능한 알맞은 경로인지
+		return (path);
+	else
+		free(path);
+	path = 0;
 	while (arr[i])
 	{
 		path = ft_strjoin(arr[i], cmd);
